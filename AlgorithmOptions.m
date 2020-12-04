42bb40999c7b674e06c644639ef877605b44021f
Ryan Wong <kinchungwong@users.noreply.github.com>
2020-12-03 23:59:11 -0800

    Update AlgorithmOptions.m
    UseParallel added true
    HashMinWindow changed from 32,32 to 19, 55
    where 19 = 9*2+1, 55 = 27*2+1

--- AlgorithmOptions.m
+++ AlgorithmOptions.m
@@ -1,7 +1,8 @@
 classdef AlgorithmOptions < handle
     properties
+        UseParallel logical {mustBeScalarOrEmpty} = true
         HashWindowSpec string = "hhhhvvv"
-        HashMinWindow {mustBeNumeric} = [32 32]
+        HashMinWindow {mustBeNumeric} = [19 55]
         HashSampleFrac {mustBeScalarOrEmpty, mustBeNumeric, mustBeInRange(HashSampleFrac, 0, 1)} = 0.1
         CommonMovementClassifier_VoteCountThreshold {mustBeScalarOrEmpty, mustBeInteger} = 2
         CommonMovementClassifier_VoteFracThreshold {mustBeScalarOrEmpty, mustBeNumeric, mustBeInRange(CommonMovementClassifier_VoteFracThreshold, 0, 1)} = 0.1
