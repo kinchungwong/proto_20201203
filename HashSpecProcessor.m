f08fa2881c1a98424d227219d2668b5880ac8b37
Ryan Wong <kinchungwong@users.noreply.github.com>
2020-12-23 07:12:58 -0800

    HashSpecProcessor.StackedOutput (default true)
    HashSpecProcessor.Process(specProc, input) will now return a stacked matrix (3-dim) where the k-th channel is the result after applying the k-th HashStep.
    
    During the transition period, this behavior can be turned off by setting HashSpecProcessor.StackedOutput to false prior to calling HashSpecProcessor.Process.
    
    In a future commit, the stacked output behavior will be controlled via HashSpec; the StackedOutput flag will be removed from HashSpecProcessor. The content of the returned stacked output will also be configured via HashSpec.

--- HashSpecProcessor.m
+++ HashSpecProcessor.m
@@ -1,21 +1,45 @@
 classdef HashSpecProcessor < handle
     properties
         HashSpec HashSpec {mustBeScalarOrEmpty}
+        StackedOutput(1, 1) logical = true
     end
     methods
         function specProc = HashSpecProcessor(hashSpec)
             specProc.HashSpec = hashSpec;
         end
+        
         function output = Process(specProc, input)
-            if ~isa(input, "U32")
-                error('input');
-            end
-            spec = specProc.HashSpec;
-            output = input;
-            for k = 1:spec.StepCount
-                step = spec.Steps(k);
-                output = HashStepProcessor(step).Process(output);
+            hashSpec = specProc.HashSpec;
+            stackedOutput = specProc.StackedOutput;
+            if isa(input, "uint32")
+                output = Internal_Process(hashSpec, input, stackedOutput);
+            elseif isa(input, "U32")
+                output = U32(Internal_Process(hashSpec, input.Data, stackedOutput));
             end
         end
+    end
+end
+
+function outputStack = Internal_Process(hashSpec, input, stackedOutput)
+    if ~isa(hashSpec, "HashSpec")
+        error('hashSpec');
+    end
+    if ~isa(input, "uint32")
+        error('input');
+    end
+    if ~islogical(stackedOutput) || ~isscalar(stackedOutput)
+        error('stackedOutput');
+    end
+    [nrows, ncols] = size(input);
+    stepCount = hashSpec.StepCount;
+    outputStack = zeros(nrows, ncols, stepCount, 'uint32');
+    for k = 1:stepCount
+        step = hashSpec.Steps(k);
+        input = HashStepProcessor(step).Process(input);
+        outputStack(:, :, k) = input;
+    end
+    if ~stackedOutput
+        warning("HashSpecProcessor.StackedOutput will default to true in a future commit.");
+        outputStack = outputStack(:, :, end);
     end
 end
