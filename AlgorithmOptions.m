f76bc773d865fc02fcabd44b7b4656cbfe00714e
Ryan Wong <kinchungwong@users.noreply.github.com>
2020-12-23 07:22:14 -0800

    AlgorithmOptions default HashWindowSpec changed
    Was: "hhhhvvv"
    Now: "hvhvhvh"
    (Effective hash input window size remains same, 27 rows x 81 cols)

--- AlgorithmOptions.m
+++ AlgorithmOptions.m
@@ -1,7 +1,7 @@
 classdef AlgorithmOptions < handle
     properties
         UseParallel logical {mustBeScalarOrEmpty} = true
-        HashWindowSpec string {mustBeTextScalar} = "hhhhvvv"
+        HashWindowSpec string {mustBeTextScalar} = "hvhvhvh"
         HashMinWindow {mustBeNumeric} = [19 55]
         HashSampleFrac {mustBeScalarOrEmpty, mustBeNumeric, mustBeInRange(HashSampleFrac, 0, 1)} = 0.1
         CMC_Options CommonMovementClassifierOptions {mustBeScalarOrEmpty}
