c7543902866c6c64cd481a3a682646b907cef97a
Ryan Wong <kinchungwong@users.noreply.github.com>
2020-12-23 06:54:35 -0800

    HashStepProcessor uses uint32 not U32
    HashStepProcessor.Process(stepProc, input) now uses uint32 for input and output.
    
    U32 deprecated; prints a warning message. A future commit will remove support for U32.

--- HashStepProcessor.m
+++ HashStepProcessor.m
@@ -6,68 +6,46 @@ classdef HashStepProcessor < handle
         function stepProc = HashStepProcessor(hashStep)
             stepProc.HashStep = hashStep;
         end
+        
         function output = Process(stepProc, input)
-            % troubleshooting
-            if true
-                output = Process_ver_2(stepProc, input);
-            else
-                output = Process_ver_1(stepProc, input);
+            if isa(input, "uint32")
+                output = Internal_Process(stepProc, input);
+            elseif isa(input, "U32")
+                % Deprecated
+                warning("Deprecated usage of U32 in HashStepProcessor.Process(stepProc, input)");
+                output = U32(Internal_Process(stepProc, input.Data));
             end
         end
     end
 end
 
-function output = Process_ver_1(stepProc, input)
-    if ~isa(input, "U32")
+function output = Internal_Process(stepProc, input)
+    if ~isa(stepProc, "HashStepProcessor")
+        error('stepProc');
+    end
+    if ~isa(input, "uint32")
         error('input');
     end
-    pts = stepProc.HashStep.RowColPoints;
-    numPts = size(pts, 1);
-    sz = size(input.Data);
-    outMargin = stepProc.HashStep.Margin;
-    outRowFirst = 1 + outMargin.Top;
-    outRowLast = sz(1) - outMargin.Bottom;
-    outColFirst = 1 + outMargin.Left;
-    outColLast = sz(2) - outMargin.Right;
-    output = U32(0, sz);
-    for outRow = outRowFirst:outRowLast
-        h = U32Hash;
-        for kp = 1:numPts
-            rowOffset = pts(kp, 1);
-            colOffset = pts(kp, 2);
-            inColFirst = outColFirst + colOffset;
-            inColLast = outColLast + colOffset;
-            h.Ingest(U32(input.Data(outRow + rowOffset, inColFirst:inColLast)));
-        end
-        output.Data(outRow, outColFirst:outColLast) = h.Data.Data;
-    end
-end
-
-function output = Process_ver_2(stepProc, input)
-    if ~isa(input, "U32")
-        error('input');
-    end
-    pts = stepProc.HashStep.RowColPoints;
-    numPts = size(pts, 1);
-    sz = size(input.Data);
+    stencils = stepProc.HashStep.RowColPoints;
+    stencilCount = size(stencils, 1);
+    sz = size(input);
     outMargin = stepProc.HashStep.Margin;
     outRowFirst = 1 + outMargin.Top;
     outRowLast = sz(1) - outMargin.Bottom;
     outColFirst = 1 + outMargin.Left;
     outColLast = sz(2) - outMargin.Right;
     outWidth = sz(2) - outMargin.Left - outMargin.Right;
-    outputData = zeros(sz, 'uint32');
+    output = zeros(sz, "uint32");
     for outRow = outRowFirst:outRowLast
-        rowData = zeros(1, outWidth, 'uint32');
-        for kp = 1:numPts
-            rowOffset = pts(kp, 1);
-            colOffset = pts(kp, 2);
+        rowData = zeros(1, outWidth, "uint32");
+        for kStencil = 1:stencilCount
+            rowOffset = stencils(kStencil, 1);
+            colOffset = stencils(kStencil, 2);
             inColFirst = outColFirst + colOffset;
             inColLast = outColLast + colOffset;
-            inputRowData = input.Data(outRow + rowOffset, inColFirst:inColLast);
+            inputRowData = input(outRow + rowOffset, inColFirst:inColLast);
             rowData = Ingest(rowData, inputRowData);
         end
-        outputData(outRow, outColFirst:outColLast) = rowData;
+        output(outRow, outColFirst:outColLast) = rowData;
     end
-    output = U32(outputData);
 end
