87027e3aa0a3987d7cd24fb16a67e5f76eab2e6d
Ryan Wong <kinchungwong@users.noreply.github.com>
2021-01-05 12:15:38 -0800

    AlgorithmOptions added HashMaskOptions field

--- AlgorithmOptions.m
+++ AlgorithmOptions.m
@@ -2,11 +2,15 @@ classdef AlgorithmOptions < handle
     properties
         UseParallel logical {mustBeScalarOrEmpty} = true
         HashOptions HashOptions {mustBeScalarOrEmpty}
+        HM_Options HashMaskOptions {mustBeScalarOrEmpty}
         CMC_Options CommonMovementClassifierOptions {mustBeScalarOrEmpty}
     end
     methods
         function opts = AlgorithmOptions()
             opts.HashOptions = HashOptions();
+            %{
+            opts.HM_Options = HashMaskOptions();
+            %}
             opts.CMC_Options = CommonMovementClassifierOptions();
         end
     end
