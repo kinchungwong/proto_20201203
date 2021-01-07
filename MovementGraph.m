6abebc74ea1275cf321f882ef8b3a0d7c17c7b44
Ryan Wong <kinchungwong@users.noreply.github.com>
2021-01-07 13:57:17 -0800

    MovementGraph uses edge table not adjacency
    MovementGraph refactored to use a list of edges (the GraphEdgeData property) instead of an adjacency matrix. This change resolves issues with edge weight processing, node filtering and reassignment, graph display, etc.
    
    The raw votes to weight formula has changed.
    
    A Subgraph() method is added.

--- MovementGraph.m
+++ MovementGraph.m
@@ -18,14 +18,26 @@ classdef MovementGraph < handle
     end
     
     properties (Access = private)
-        % Weighted adjacency matrix.
+        % graphEdgeData contains individual records of graph edges and
+        % associated data, with each row containing [a, b, dr, dc, w]
+        % where 
+        % ... (a, b) = the image pair (the a-th and b-th images)
+        % ... (dr) = the row movement (delta) between (a) and (b)
+        % ... (dc) = the column movement (delta) between (a) and (b)
+        % ... (w) = the confidence (weight) of the edge
         %
-        % First plane, M(:, :, 1), is the weight, computed from votes.
-        %
-        % Second and third plane, M(:, :, 2:3), is the best movement vector
-        % associated with the current weight.
-        %
-        M(:, :, 3) int32
+        % This matrix may contain multiple rows where (a, b) are 
+        % identical. When such rows exist, the row that has the highest 
+        % weight (w) is the row that will be retained after post-process 
+        % filtering.
+        % 
+        GraphEdgeData(:, 5) int32 = zeros(0, 5, "int32")
+        
+        % Graph
+        G {IsScalarOrEmpty_GraphObj}
+        
+        % ConnComps
+        CC
     end
     
     properties (Dependent)
@@ -41,6 +53,11 @@ classdef MovementGraph < handle
         
         % The weights for each edge in Edges.
         % Weights is an N-by-1 vector.
+        %
+        % The weight values are computed using a formula that considers
+        % both the raw votes in CommonMovementClassifier, the number of
+        % images used as input in that CommonMovementClassifier, among
+        % other factors.
         Weights
         
         % The movement vectors between a pair of images represented 
@@ -48,6 +65,13 @@ classdef MovementGraph < handle
         % Deltas is an N-by-2 matrix, each row containing 
         % (rowDelta, colDelta) for the same row in Edges.
         Deltas
+        
+        % A MATLAB Graph object of connections between pairs of images.
+        Graph
+        
+        % A cell vector containing each set of connected components found
+        % in the graph.
+        ConnComps
     end
     
     methods
@@ -62,8 +86,6 @@ classdef MovementGraph < handle
             end
             mg.Cache = cache;
             mg.Ids = cache.Ids;
-            numIds = length(mg.Ids);
-            mg.M = zeros(numIds, numIds, 3, "uint32");
         end
         
         function Process(mg)
@@ -72,62 +94,74 @@ classdef MovementGraph < handle
             arguments
                 mg(1, 1) MovementGraph
             end
-            Internal_Process(mg);
+            mg.GraphEdgeData = Internal_Process(mg);
+            [mg.G, mg.CC] = Internal_CreateGraph(mg);
         end
         
-        function m = get.Adjacency(mg)
+        function ab = get.Edges(mg)
             arguments
                 mg(1, 1) MovementGraph
             end
-            m = mg.M(:, :, 1);
-        end
-        
-        function ij = get.Edges(mg)
-            arguments
-                mg(1, 1) MovementGraph
-            end
-            [i, j] = find(mg.M(:, :, 1));
-            ij = cat(2, int32(i), int32(j));
+            ab = mg.GraphEdgeData(:, 1:2);
         end
         
         function w = get.Weights(mg)
             arguments
                 mg(1, 1) MovementGraph
             end
-            m = mg.M(:, :, 1);
-            w = m(m > 0);
+            w = mg.GraphEdgeData(:, 5);
         end
         
-        function d = get.Deltas(mg)
+        function drdc = get.Deltas(mg)
             arguments
                 mg(1, 1) MovementGraph
             end
-            m = mg.M(:, :, 1);
-            r = mg.M(:, :, 2);
-            c = mg.M(:, :, 3);
-            r = r(m > 0);
-            c = c(m > 0);
-            d = cat(2, r, c);
+            drdc = mg.GraphEdgeData(:, 3:4);
         end
         
-        function g = graph(mg)
+        function g = get.Graph(mg)
             % Converts the result into a graph.
             % See also: graph
             arguments
                 mg(1, 1) MovementGraph
             end
-            g = graph(double(mg.Adjacency), "upper");
+            g = mg.G;
         end
         
-        function plot(mg, varargin)
-            % Creates a plot of the connectivity graph.
-            
-            %{
+        function mab = get.Adjacency(mg)
             arguments
                 mg(1, 1) MovementGraph
             end
-            %}
-            plot(graph(mg), varargin{:});
+            mab = mg.G.adjacency();
+        end
+        
+        function [g, cc] = Subgraph(mg, ids, ids2)
+            arguments
+                mg(1, 1) MovementGraph
+                ids(1, :) int32
+                ids2(1, :) int32 = []
+            end
+            [g, cc] = Internal_CreateSubgraph(mg, ids, ids2);
+        end
+        
+        function Plot(mg)
+            % Creates a plot of the connectivity graph.
+            arguments
+                mg(1, 1) MovementGraph
+            end
+            g = mg.Graph;
+            weights = mg.Weights;
+            deltas = mg.Deltas;
+            wd = cat(2, deltas, weights);
+            strEdgeLabels = string(num2str(wd, "%d "));
+            plot(g, "EdgeLabel", strEdgeLabels);
+        end
+        
+        function cc = get.ConnComps(mg)
+            arguments
+                mg(1, 1) MovementGraph
+            end
+            cc = mg.CC;
         end
     end
     
@@ -173,45 +207,108 @@ classdef MovementGraph < handle
     end    
 end
 
-function Internal_Process(mg)
+function edgeData = Internal_Process(mg)
     arguments
         mg(1, 1) MovementGraph
     end
-    allIds = mg.Ids;
-    keys = mg.Cache.Keys;
-    numCMC = length(keys);
-    for kCMC = 1:numCMC
-        thisIds = keys{kCMC};
+    
+    % Incrementally populated GraphEdgeData. 
+    % This matrix is preallocated, where edgeCount tracks
+    % the number of valid rows of data.
+    edgeData = zeros(100, 5, "int32");
+
+    % Current number of valid rows inside (edgeData).
+    edgeCount = 0;
+   
+    % The cache contains instances of CommonMovementClassifier.
+    % Each instance is keyed by the set of image IDs used in its
+    % computation.
+    %
+    cacheKeys = mg.Cache.Keys;
+    cacheCount = length(cacheKeys);
+    for cacheIndex = 1:cacheCount
+        thisIds = cacheKeys{cacheIndex};
+        thisImageCount = length(thisIds);
+        
+        % Though impossible, this has to be catched, otherwise subsequent
+        % operations may have unintended consequences.
+        if thisImageCount < 2
+            continue;
+        end
+        
         cmc = mg.Cache.Get(thisIds);
         [bestVote, bestDeltas] = BestMovement(cmc);
         if bestVote == 0 || isempty(bestDeltas)
             continue;
         end
-        thisWeight = bestVote * length(thisIds);
-        for kPair = 1:(length(thisIds)-1)
-            ka = thisIds(kPair);
-            kb = thisIds(kPair + 1);
-            % ---
-            % Convert image IDs into matrix coordinate
-            %
-            % (For image IDs that are sequentially assigned starting at 1,
-            % this step is idempotent. However, the former condition isn't
-            % guaranteed.)
-            % ---
-            ka = find(allIds == ka, 1, "first");
-            kb = find(allIds == kb, 1, "first");
-            rowDelta = bestDeltas(1, kPair * 2 - 1);
-            colDelta = bestDeltas(1, kPair * 2);
-            if thisWeight > mg.M(ka, kb, 1)
-                mg.M(ka, kb, 1) = thisWeight;
-                mg.M(ka, kb, 2) = rowDelta;
-                mg.M(ka, kb, 3) = colDelta;
+        
+        % The weight is modified from CMC vote count, so that CMC computed
+        % using a larger number of input images will have a boosted weight.
+        %
+        % This boosting is intended to compensate for the fact that, when a
+        % CMC is computed using more input images, the amount of identical
+        % image content occurring across all input image is reduced due to
+        % content being shifted away, sometimes reduced to zero.
+        %
+        % Currently the boosting is computed as follows:
+        % (CMC inputs) ... (boosting)
+        %  2 images ... votes * 1.0
+        %  3 images ... votes * 1.25
+        %  4 images ... votes * 1.67
+        %  5 images ... votes * 2.5
+        %
+        thisWeight = round(bestVote * 5 / (7 - thisImageCount));
+        if thisWeight == 0
+            continue;
+        end
+
+        for whichPair = 1:thisImageCount-1
+            % Image index of each pair in the input
+            ka = thisIds(whichPair);
+            kb = thisIds(whichPair + 1);
+            
+            % Movement between the two images
+            rowDelta = bestDeltas(1, whichPair * 2 - 1);
+            colDelta = bestDeltas(1, whichPair * 2);
+            
+            % Insert into new row; reallocate matrix if necessary.
+            edgeCount = edgeCount + 1;
+            while edgeCount > size(edgeData, 1)
+                dsz = size(edgeData);
+                dcn = class(edgeData);
+                edgeData = cat(1, edgeData, zeros(dsz, dcn));
             end
+            
+            % Write data into new row
+            edgeData(edgeCount, :) = [ka, kb, rowDelta, colDelta, thisWeight];
         end
     end
+    
+    % Truncate unused preallocated rows from the matrix
+    edgeData = edgeData(1:edgeCount, :);
+
+    % ---
+    % When multiple identical edge data exist between two images, where 
+    % "identical" means between the same two images and the same movement
+    % deltas, we can take the row with the highest weight, and discard the
+    % duplicates.
+    %
+    % Edges between the same two images but with different movement deltas
+    % will be treated as different edges.
+    % 
+    % To maintain correspondence with the MATLAB graph, it is a requirement 
+    % that the GraphEdgeData matrix is always lexicographically sorted.
+    % This is handled by Internal_DedupeEdgeData as well.
+    % 
+    edgeData = Internal_DedupeEdgeData(edgeData);
 end
 
 function [bestVote, bestDeltas] = BestMovement(cmc)
+    % Given an instance of CommonMovementClassifier (CMC), find the 
+    % highest voted movement whose deltas (vectors of row and column 
+    % movement) that is not entirely zero across all input images 
+    % to the CMC.
+    %
     arguments
         cmc(1, 1) CommonMovementClassifier
     end
@@ -231,4 +328,90 @@ function [bestVote, bestDeltas] = BestMovement(cmc)
             bestDeltas = thisDeltas;
         end
     end
+end
+
+function tf = IsScalarOrEmpty_GraphObj(g)
+    if isempty(g)
+        tf = true;
+    elseif isa(g, "graph")
+        tf = true;
+    elseif iscell(g) && isscalar(g) && isa(g{1}, "graph")
+        tf = true;
+    else
+        tf = false;
+    end
+end
+
+function [g, cc] = Internal_CreateGraph(mg)
+    arguments
+        mg(1, 1) MovementGraph
+    end
+    a = mg.GraphEdgeData(:, 1);
+    b = mg.GraphEdgeData(:, 2);
+    w = mg.GraphEdgeData(:, 5);
+    g = graph(single(a), single(b), single(w));
+    cc = conncomp(g, "OutputForm", "cell");
+end
+
+function [g, cc] = Internal_CreateSubgraph(mg, ids, ids2)
+    arguments
+        mg(1, 1) MovementGraph
+        ids(1, :) int32
+        ids2(1, :) int32 = []
+    end
+    hasIds2 = isequal(length(ids2), length(ids));
+    if ~isempty(ids2) && ~hasIds2
+        error('Incorrect vector length for ID reassignment argument ids2.');
+    end
+    data = mg.GraphEdgeData;
+    m = logical(ismember(data(:, 1:2), ids));
+    m = logical(m(:, 1) & m(:, 2));
+    data = data(m, :);
+    data = sortrows(data);
+    a = data(:, 1);
+    b = data(:, 2);
+    w = data(:, 5);
+    if hasIds2
+        idsBimap = SmallDenseIntegerBimap(ids, ids2);
+        a = idsBimap.LtoR(a);
+        b = idsBimap.LtoR(b);
+    end
+    g = graph(single(a), single(b), single(w));
+    cc = conncomp(mg.G, "OutputForm", "cell");
+end
+
+function edgeData = Internal_DedupeEdgeData(edgeData)
+    arguments
+        edgeData(:, 5) int32
+    end
+    count = size(edgeData, 1);
+    if count == 0
+        return;
+    end
+    a = "ascend";
+    d = "descend";
+    sortKeys = [1, 2, 3, 4, 5];
+    sortDirs = [a, a, a, a, d];
+    edgeData = sortrows(edgeData, sortKeys, sortDirs);
+    
+    % ---
+    % Compact the rows of the edgeData matrix, so that, 
+    % for consecutive groups of rows with same image pair and 
+    % row-column deltas, only the row with the highest weight
+    % will be retained in the final edgeData matrix.
+    % ---
+    outCount = 1;
+    for kin = 2:count
+        if isequal(edgeData(kin, 1:4), edgeData(outCount, 1:4))
+            continue;
+        else
+            outCount = outCount + 1;
+            if outCount ~= kin
+                edgeData(outCount, :) = edgeData(kin, :);
+            end
+        end
+    end
+    
+    % Finish row compaction
+    edgeData = edgeData(1:outCount, :);
 end
